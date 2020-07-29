e9916ba622d3b0c6630e8f846ed73b7f95490290
Nayeli Garcia <nayeli.garcia0103@gmail.com>
2020-07-29 13:43:40 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { CategoriasComponent } from './pages/categorias/categorias.component';
+import { ColoresComponent } from './pages/colores/colores.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'categorias', component: CategoriasComponent },
+  { path: 'colores', component: ColoresComponent },
+  { path: '*', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { CategoriasComponent } from './pages/categorias/categorias.component';
+import { ColoresComponent } from './pages/colores/colores.component';`
+`
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UsuariosComponent,
+    ProductosComponent,
+    CategoriasComponent,
+    ColoresComponent
   ],
   imports: [
     BrowserModule,
